4c5ec96de610d3f51ada8977dd560f03403b9d3d
Stefano <stefano.sc.ciapponi@gmail.com>
2024-11-20 11:31:05 +0100

    modified:   config/base.yaml 	modified:   main.py 	new file:   model/decoder.py 	new file:   model/quant_decoder_components.ckpt 	modified:   requirements-dbclient.txt 	new file:   utils.py

--- DBClient/main.py
+++ DBClient/main.py
@@ -1,54 +1,65 @@
 import paho.mqtt.client as mqtt
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
 import hydra 
 import json 
+import sys
+from model.decoder import QuantDecoder
+import whisper 
+import torch 
+from utils import FIFOBuffer, WhisperDecodingThreadWrapper
 
 @hydra.main(version_base=None, config_path='./config', config_name='base')
 def main(args):
+
+    # LOAD DECODER MODEL
+    decoder = QuantDecoder(args.NAC.MODEL_PATH)
+    
+    # LOAD WHISPER
+    wmodel = whisper.load_model("turbo").cuda()
+    print("loaded!")
     # Mosquitto MQTT Configuration
     MQTT_BROKER = args.MQTT.BROKER_ADDRESS  # Replace with your MQTT broker address
     MQTT_PORT = 1883
     MQTT_TOPIC = "AUDIOLOGGER"  # Example topic to subscribe to
 
-    # InfluxDB Configuration
-    INFLUXDB_URL = args.INFLUXDB.URL 
-    INFLUXDB_TOKEN = args.INFLUXDB.TOKEN
-    INFLUXDB_ORG = args.INFLUXDB.ORG
-    INFLUXDB_BUCKET = args.INFLUXDB.BUCKET
-
-    # Initialize InfluxDB Client
-    influxdb_client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN)
-    write_api = influxdb_client.write_api(write_options=SYNCHRONOUS)
 
     # Callback when the client connects to the broker
     def on_connect(client, userdata, flags, rc):
         print(f"Connected to MQTT Broker with code {rc}")
         client.subscribe(MQTT_TOPIC)
 
+    # Buffer to hold the indices
+    buffer = FIFOBuffer(6)
     # Callback when a message is received from the broker
     def on_message(client, userdata, msg):
+     # Ensure we can modify the global buffer
+        
         try:
             # Decode and parse the message
             payload = msg.payload.decode()
-            # print(f"Received message: {payload} on topic {msg.topic}")
             
             # Convert the payload to a data point
-            # Example assumes JSON payload with fields 'temperature' and 'timestamp'
-            data = json.loads(payload)  # Replace eval with a safer parsing method (e.g., json.loads) for JSON
+            data = json.loads(payload)  # Safely parse JSON
+            indices = data["data"]
+            tensor_indices = torch.Tensor(indices).int()
             
-            # Create a Point for InfluxDB
-            point = (
-                Point("audiolog")  # Measurement name
-                .field("audio", str(data["data"]))  # Field
-                .time(data["timestamp"])  # Timestamp
-            )
-            
-            # Write to InfluxDB
-            write_api.write(bucket=INFLUXDB_BUCKET, org=INFLUXDB_ORG, record=point)
-            print(f"Written to InfluxDB: {point}")
+            # Append the tensor to the buffer
+            buffer.enqueue(tensor_indices)
+                
         except Exception as e:
             print(f"Error processing message: {e}")
+        except KeyboardInterrupt:
+            print("Stream Ended")
+            sys.exit(0)
+
+    def start_thread(condition):
+        if(condition and not wthread.is_alive()):
+            wthread.start()
+        else:
+            wthread.stop()
+
+    wthread = WhisperDecodingThreadWrapper(buffer, decoder, wmodel)
+    wthread.daemon = True
+    wthread.start()
 
     # Initialize MQTT Client
     mqtt_client = mqtt.Client()
@@ -63,6 +74,7 @@ def main(args):
 
     # Start MQTT loop
     mqtt_client.loop_forever()
+    wthread.stop()
 
 if __name__=="__main__":
     main()
